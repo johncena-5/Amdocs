1ea2aa4cbbe9a6f9d22620794364f117ce4c76d8
johncena-5 <warisansari5219@gmail.com>
2023-10-22 23:16:55 +0530

    second commit

--- src/test/java/Basic.java
+++ src/test/java/Basic.java
@@ -2,11 +2,12 @@ public class Basic {
 
     public static void main(String[] args) {
 
-        int[] a={1,2,3,4};
-        for (int i=0;i<a.length;i++){
-            if(a[i]%2==0){
+        int[] a = {1, 2, 3, 4};
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
                 System.out.println(a[i]);
             }
         }
+        System.out.println("java");
     }
 }
